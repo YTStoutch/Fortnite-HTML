05b57ec6790e34b9a0925dd2c1ba54034f722afb
YTStoutch <jeux56330@gmail.com>
2025-12-05 21:44:25 +0100

    Update serveur.js

--- serveur.js
+++ serveur.js
@@ -3,25 +3,34 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+// Sert index.html et tout le dossier
 app.use(express.static('.'));
 
+// Liste des joueurs connectés
 let players = {};
 
+// Quand un joueur se connecte
 io.on('connection', socket => {
   console.log('Player connected:', socket.id);
-  players[socket.id] = {x:0,y:0,z:0,rotationY:0};
+  players[socket.id] = {x:0, y:0, z:0, rotationY:0};
+
+  // Envoie la liste de tous les joueurs
   io.emit('playersUpdate', players);
 
+  // Reçoit la position d’un joueur
   socket.on('updatePosition', data => {
     players[socket.id] = data;
     io.emit('playersUpdate', players);
   });
 
+  // Joueur déconnecté
   socket.on('disconnect', () => {
+    console.log('Player disconnected:', socket.id);
     delete players[socket.id];
     io.emit('playersUpdate', players);
   });
 });
 
+// Écoute sur le port fourni par Render
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => console.log(`Server running on port ${PORT}`));
